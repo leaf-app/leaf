9cabdd318492fbd759b157a48b44b6f6372beaa5
dzgeorgy <dzgeorgy@gmail.com>
2020-06-26 22:06:54 +0300

    Remove viewmodel in activity
    Signed-off-by: dzgeorgy <dzgeorgy@gmail.com>

--- auth/src/main/java/ru/dzgeorgy/auth/ui/activity/LoginActivity.kt
+++ auth/src/main/java/ru/dzgeorgy/auth/ui/activity/LoginActivity.kt
@@ -3,14 +3,12 @@ package ru.dzgeorgy.auth.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import ru.dzgeorgy.auth.LoginViewModel
 import ru.dzgeorgy.auth.databinding.ActivityLoginBinding
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
