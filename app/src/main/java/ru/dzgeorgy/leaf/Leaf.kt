d3eedf0e2d453affbf01bfcf256145bf23fcad85
dzgeorgy <dzgeorgy@gmail.com>
2020-07-01 23:35:17 +0300

    Remove explicit start of LoginActivity on application start
    Signed-off-by: dzgeorgy <dzgeorgy@gmail.com>

--- app/src/main/java/ru/dzgeorgy/leaf/Leaf.kt
+++ app/src/main/java/ru/dzgeorgy/leaf/Leaf.kt
@@ -1,12 +1,10 @@
 package ru.dzgeorgy.leaf
 
 import android.app.Application
-import android.content.Intent
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import dagger.hilt.android.HiltAndroidApp
-import ru.dzgeorgy.auth.ui.activity.LoginActivity
 
 @HiltAndroidApp
 class Leaf : Application() {
@@ -17,11 +15,7 @@ class Leaf : Application() {
             setDefaultsAsync(ru.dzgeorgy.core.R.xml.remote_config_defaults)
             if (BuildConfig.DEBUG) remoteConfigSettings { minimumFetchIntervalInSeconds = 3600 }
         }.fetchAndActivate().addOnCompleteListener {
-            startActivity(
-                Intent(
-                    this,
-                    LoginActivity::class.java
-                ).also { it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) })
+
         }
     }
 
