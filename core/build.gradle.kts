69602394afb00d4b49154a593d1b3a7e68865198
dzgeorgy <dzgeorgy@gmail.com>
2020-07-01 23:48:18 +0300

    Remove navigation library from core module
    Signed-off-by: dzgeorgy <dzgeorgy@gmail.com>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -36,8 +36,6 @@ dependencies {
     api(Dependencies.Libs.hiltAndroid)
     api(Dependencies.Libs.kotlinCoroutines)
     api(Dependencies.Libs.kotlinSerialization)
-    api(Dependencies.Libs.navigation)
-    api(Dependencies.Libs.navigationUi)
 
     //Annotation processors
     kapt(Dependencies.AnnotationProcessors.hilt)
