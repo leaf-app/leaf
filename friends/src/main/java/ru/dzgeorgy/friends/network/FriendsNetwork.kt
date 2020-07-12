7d7936570c517d12b2d760cb9adf8e84a662bafc
dzgeorgy <dzgeorgy@gmail.com>
2020-07-12 17:30:01 +0300

    Create network interface for friends section in VK API
    Signed-off-by: dzgeorgy <dzgeorgy@gmail.com>

---
+++ friends/src/main/java/ru/dzgeorgy/friends/network/FriendsNetwork.kt
@@ -0,0 +1,19 @@
+package ru.dzgeorgy.friends.network
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.dzgeorgy.core.network.Network
+
+interface FriendsNetwork {
+
+    @GET("friends.get")
+    suspend fun get(
+        @Query("user_id") userId: Int,
+        @Query("offset") offset: Int,
+        @Query("count") count: Int,
+        @Query("access_token") token: String,
+        @Query("fields") fields: String = "photo_max",
+        @Query("order") order: String = "name"
+    ): Network.ResponseArray<FriendItem>
+
+}
